95544bd1316c0d7e7746ba7c55f9269bab4f0e55
SpottieTea <nsdodson@miners.utep.edu>
2018-09-22 02:05:34 -0600

    revising code to read file input.

--- file-transfer-lab/labSocket.py
+++ file-transfer-lab/labSocket.py
@@ -2,16 +2,20 @@ import re
 
 
 
-def framedSend(sock, payload, debug=0):
-    if debug: print("framedSend: sending %d byte message" % len(payload))
+def fileSend(sock, payload):
+    
     filein = open(payload,"r")
     for l in filein:
         
-        msg = str(len(l)).encode() + b':' + l
+        msg = l
         while len(msg):
             nsent = sock.send(msg)
-            msg = msg[nsent:]
 
 
 
-rbuf = b""                      # static receive buffer
+def recieveFile(sock):
+
+
+    fileopen = open("serverFile.txt","w")
+    r = sock.recv(100)
+    fileopen.write(r)
