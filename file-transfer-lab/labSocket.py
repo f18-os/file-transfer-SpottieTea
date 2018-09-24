71f5d0ee6ece0207556344ce67e1abc903f7f8eb
SpottieTea <nsdodson@miners.utep.edu>
2018-09-23 22:21:13 -0600

    Client now sends entire file!

--- file-transfer-lab/labSocket.py
+++ file-transfer-lab/labSocket.py
@@ -5,18 +5,20 @@ import re
 def fileSend(sock, payload):
     
     filein = open(payload,"rb")
-    for l in filein: #for each line, send over socket
-        
+    lines = filein.readlines()
+    for l in lines: #for each line, send over socket
+        print(l)
+        print("\n")
         msg = l
-        while len(msg):
-            nsent = sock.send(msg)
+        nsent = sock.send(msg)
 
 
 
 def receiveFile(sock):
-
+    
     fileopen = open("serverText.txt", "w")
     r = sock.recv(100) #receive bytes
     line = r.decode('ascii') #decode bytes
     print(line) #check if recieved
     fileopen.write(line) #write to file
+    
