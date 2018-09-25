7fc9e381a6f7b6a198148d2e4f78366dcadef762
SpottieTea <nsdodson@miners.utep.edu>
2018-09-24 19:08:31 -0600

    Modifications to recieveFile

--- file-transfer-lab/labSocket.py
+++ file-transfer-lab/labSocket.py
@@ -3,8 +3,10 @@ import re
 
 
 def fileSend(sock, payload):
-    
+
+    #nsent = sock.send(payload.encode())
     filein = open(payload,"rb")
+    
     lines = filein.readlines()
     for l in lines: #for each line, send over socket
         print(l)
@@ -14,10 +16,18 @@ def fileSend(sock, payload):
 
 
 def receiveFile(sock):
+
+    r = sock.recv(100)
+    line = r.decode('ascii')
+    fileopen = open("serverText.txt","w")
+    fileopen.write(line)
+    r = sock.recv(100)
     
-    fileopen = open("serverText.txt", "w")
-    r = sock.recv(100) #receive bytes
-    line = r.decode('ascii') #decode bytes
-    print(line) #check if recieved
-    fileopen.write(line) #write to file
+    while r:
+        line = r.decode('ascii') #decode bytes
+        print(line) #check if recieved
+        fileopen.write(line) #write to file
+        r = sock.recv(100)
+
+    
     
